e3ef4b5f58fb1611f87b21c136b62376768858b0
A-San96 <assanediatta96@gmail.com>
2023-12-10 14:53:27 +0000

    feat: Add abstract file for linked list data structure

---
+++ DataStructAndAlgorithm/Abstractions/LinkedListDS/ILinkedList.cs
@@ -0,0 +1,14 @@
+﻿namespace DataStructAndAlgorithm;
+
+public interface ILinkedList<T>
+{
+    int Length { get; }
+
+    void InsertAt(T item, int index);
+    T Remove(T item);
+    T RemoveAt(int index);
+    void Append(T item);
+    void Prepend(T item);
+    T Get(int index);
+
+}
---
+++ DataStructAndAlgorithm/Abstractions/LinkedListDS/Node.cs
@@ -0,0 +1,13 @@
+﻿namespace DataStructAndAlgorithm;
+
+public class Node<T>
+{
+    public Node(T value)
+    {
+        Value = value;
+    }
+
+    public T Value { get; set; }
+    public Node<T>? Next { get; set; }
+    public Node<T>? Prev { get; set; }
+}

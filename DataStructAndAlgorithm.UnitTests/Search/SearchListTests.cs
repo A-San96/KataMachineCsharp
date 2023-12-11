039dcb3493e2078b2c0c54d8bf0089e8837b29fe
A-San96 <assanediatta96@gmail.com>
2023-12-11 15:49:30 +0000

    refactor: rename namespace for a better preview of test list on IDE

--- DataStructAndAlgorithm.UnitTests/Search/SearchListTests.cs
+++ DataStructAndAlgorithm.UnitTests/Search/SearchListTests.cs
@@ -1,43 +1,42 @@
 ﻿using DataStructAndAlgorithm.Implementations.Search;
 
-namespace DataStructAndAlgorithm.UnitTests.Search
+namespace DataStructAndAlgorithm.UnitTests;
+
+public class SearchListTests
 {
-    public class SearchListTests
+    private SearchList _searchList;
+    public SearchListTests()
     {
-        private SearchList _searchList;
-        public SearchListTests()
-        {
-            _searchList = new SearchList();
-        }
+        _searchList = new SearchList();
+    }
 
-        [Fact]
-        public void LinearSearchTest()
-        {
-            // Arrange
-            int[] foo = [1, 3, 4, 69, 71, 81, 90, 99, 420, 1337, 69420];
+    [Fact]
+    public void LinearSearchTest()
+    {
+        // Arrange
+        int[] foo = [1, 3, 4, 69, 71, 81, 90, 99, 420, 1337, 69420];
 
-            // Act & Assert
-            Assert.True(_searchList.LinearSearch(foo, 69));
-            Assert.False(_searchList.LinearSearch(foo, 1336));
-            Assert.True(_searchList.LinearSearch(foo, 69420));
-            Assert.False(_searchList.LinearSearch(foo, 69421));
-            Assert.True(_searchList.LinearSearch(foo, 1));
-            Assert.False(_searchList.LinearSearch(foo, 0));
-        }
+        // Act & Assert
+        Assert.True(_searchList.LinearSearch(foo, 69));
+        Assert.False(_searchList.LinearSearch(foo, 1336));
+        Assert.True(_searchList.LinearSearch(foo, 69420));
+        Assert.False(_searchList.LinearSearch(foo, 69421));
+        Assert.True(_searchList.LinearSearch(foo, 1));
+        Assert.False(_searchList.LinearSearch(foo, 0));
+    }
 
-        [Fact]
-        public void BinarySearchTest()
-        {
-            // Arrange
-            int[] foo = [1, 3, 4, 69, 71, 81, 90, 99, 420, 1337, 69420];
+    [Fact]
+    public void BinarySearchTest()
+    {
+        // Arrange
+        int[] foo = [1, 3, 4, 69, 71, 81, 90, 99, 420, 1337, 69420];
 
-            // Act & Assert
-            Assert.True(_searchList.BinarySearch(foo, 69));
-            Assert.False(_searchList.BinarySearch(foo, 1336));
-            Assert.True(_searchList.BinarySearch(foo, 69420));
-            Assert.False(_searchList.BinarySearch(foo, 69421));
-            Assert.True(_searchList.BinarySearch(foo, 1));
-            Assert.False(_searchList.BinarySearch(foo, 0));
-        }
+        // Act & Assert
+        Assert.True(_searchList.BinarySearch(foo, 69));
+        Assert.False(_searchList.BinarySearch(foo, 1336));
+        Assert.True(_searchList.BinarySearch(foo, 69420));
+        Assert.False(_searchList.BinarySearch(foo, 69421));
+        Assert.True(_searchList.BinarySearch(foo, 1));
+        Assert.False(_searchList.BinarySearch(foo, 0));
     }
 }
